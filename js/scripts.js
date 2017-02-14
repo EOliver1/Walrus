084cda5443be32fd48f8f56e0ace9600df957cf9
Elizabeth Oliver and Nathaniel Meyer <elizabeth@>
2017-02-14 10:28:51 -0800

    Add bucket chronicles class with js for styling.

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,17 @@ $(document).ready(function() {
   });
   $(".otherClickable").click(function() {
     $("#bucketChronicles").fadeToggle();
+    $("#bucketChronicles").removeClass();
+  });
+  $("button#night").click(function() {
+    $("body").removeClass();
+    $("body").addClass("night-mode");
+  });
+  $("button#day").click(function() {
+    $("body").removeClass();
+    $("body").addClass("day-mode");
+  });
+  $(".highlight").click(function() {
+    $("#bucketChronicles").toggleClass("paragraphStylez");
   });
 });
